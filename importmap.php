a043ae61d79da07178e5ab817dd5a5e165ba424e
cd-dist-2324 <cd.dist.2324@gmail.com>
2026-03-18 11:51:59 +0100

    Registration form

--- importmap.php
+++ importmap.php
@@ -26,4 +26,13 @@ return [
         'version' => '5.3.8',
         'type' => 'css',
     ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@symfony/stimulus-bundle' => [
+        'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '7.3.0',
+    ],
 ];
