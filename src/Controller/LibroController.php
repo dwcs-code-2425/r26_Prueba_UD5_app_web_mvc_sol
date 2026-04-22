ffb313d3575bb9aded084742737475d09be720e0
cd-dist-2324 <cd.dist.2324@gmail.com>
2026-04-22 11:44:48 +0200

    ++listado libros + botón buscar en otro servicio web

--- src/Controller/LibroController.php
+++ src/Controller/LibroController.php
@@ -10,25 +10,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class LibroController extends AbstractController
 {
-   
+
 
     #[Route('/libros/buscar', name: 'libro_buscar')]
     public function buscar(Request $request, LibroRepository $libroRepository): Response
     {
         // Obtenemos los parámetros de la query
-        $titulo   = $request->query->get('titulo');   // devuelve string o null
+        $titulo = $request->query->get('titulo');   // devuelve string o null
         $unidades = $request->query->get('unidades'); // siempre string si se pasa
-        $autor    = $request->query->get('autor');    // string o null
+        $autor = $request->query->get('autor');    // string o null
 
         // Manejar cadenas vacías o parámetros no enviados
-        $titulo   = !empty($titulo) ? $titulo : null;
+        $titulo = !empty($titulo) ? $titulo : null;
         $unidades = !empty($unidades) ? (int) $unidades : null; // convertimos a int
-        $autor    = !empty($autor) ? $autor : null;
+        $autor = !empty($autor) ? $autor : null;
 
         // Ahora podemos usar estos valores para buscar en el repositorio
-      
+
         $libros = $libroRepository->buscarLibros($titulo, $unidades, $autor);
 
         return $this->render("libro/busqueda.html.twig", ["libros" => $libros]);
+    }
+ #[Route('/libros', name: 'libro_index')]
+    public function index(LibroRepository $libroRepository): Response
+    {
+        $libros = $libroRepository->findAll();
+        return $this->render("libro/index.html.twig", ["libros" => $libros]);
     }
 }
